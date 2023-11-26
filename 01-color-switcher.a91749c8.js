c3a398d65f4127e0dd319126ae4db444b643d4ff
troanart <128319062+troanart@users.noreply.github.com>
2023-11-26 12:37:16 +0000

    Deploying to gh-pages from @ troanart/goit-js-hw-09@86275c32ca2c00804ba2a36938cceff6ec8273bd 🚀

---
+++ 01-color-switcher.a91749c8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector('button[data-start="start"]'),a=document.querySelector('button[data-stop="stop"]'),n=null;a.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,a.disabled=!1,n=setInterval((function(){t.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)))}),1e3)})),a.addEventListener("click",(function(t){e.disabled=!1,a.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.a91749c8.js.map
